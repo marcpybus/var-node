56a2f003124fea36f7220f1ed7fa41acbdd054f0
labgenfp2 <Marc Pybus>
2024-04-29 17:02:42 +0200

    nginx config fix

--- data-manager/docker-entrypoint.sh
+++ data-manager/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Waiting 10 seconds.."
-sleep 10
+echo "Waiting 30 seconds.."
+sleep 30
 
 echo "Fetching VEP caches, Fasta files and liftOver chain files for GRCh37 and GRCh38 genomes (download size ~46Gb):"
 
